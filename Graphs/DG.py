0a2432fe23a55106a8c8f4d96fb0fbc6eb02ea09
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2019-11-15 06:27:54 +0500

    Add files via upload

---
+++ Graphs/DG.py
@@ -0,0 +1,95 @@
+class digraph:
+    def __init__(self):
+        self.g = {}
+
+    def add_node(self, node):
+        if node in self.g:
+            raise ValueError("node already exists")
+
+        self.g[node] = []
+
+    def add_edge(self, src, dest):
+        if src not in self.g:
+            raise ValueError("Source doesn't exist")
+
+        if dest not in self.g:
+            raise ValueError("Destination doesn't exist")
+
+        nexts = self.g[src]
+        if dest in nexts:
+            return
+
+        nexts.append(dest)
+
+
+    def traversal(self, start):
+        q = [start]
+        visited = []
+
+        while q:
+            current = q.pop(0)
+
+            if current in visited:
+                continue
+
+            print(current)
+
+            visited.append(current)
+
+            next_nodes = self.g[current]
+            for n in next_nodes:
+                q.append(n)
+
+
+    def __str__(self):
+        return self.g
+
+
+    def find_path(self, start, end, path = []):
+        if start not in self.g:
+            raise ValueError("Start doesn't exist my friend")
+
+        path.append(start)
+
+        if start == end:
+            return path
+
+        for node in self.g[start]:
+            if node not in path:
+                new_path = self.find_path(node, end, path)
+
+                if new_path:
+                    return new_path
+
+        
+        return None
+
+
+
+
+
+
+d = digraph()
+d.add_node("a")
+d.add_node("b")
+d.add_node("c")
+d.add_node("d")
+d.add_node("e")
+d.add_node("f")
+d.add_edge("a","b")
+d.add_edge("a","c")
+d.add_edge("b","d")
+d.add_edge("c","e")
+d.add_edge("c","f")
+d.add_edge("b", "f")
+
+print(d.find_path("a", "f"))
+
+
+# print(d)
+# print(d.__str__())
+
+
+
+
+
